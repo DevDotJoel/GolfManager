707200a02fcf6ec8a444f329f3105b403cdc7729
Joel Ferreira <30007769@students.ual.pt>
2023-03-29 18:35:10 +0100

    added entities

---
+++ GolfManager.Domain/Entities/Field.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolfManager.Domain.Entities
+{
+    public class Field
+    {
+        [Key]
+        public int Id { get;  set; }
+        public string Name { get;  set; }
+        public string Description { get; set; }
+        public int Capacity { get; set; }
+    }
+}
